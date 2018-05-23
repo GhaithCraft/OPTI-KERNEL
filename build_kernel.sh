28c36b3330032a889c0ba42141fcc36e0baa6db4
GhaithCraft <GhaithCraft@Gmail.com>
2018-05-23 20:35:11 +0300

    setup OPTI-KERNEL

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=/home/ghaith/android/Toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
 
-make exynos7880-a7y17lte_defconfig
+make Optikernel_a7y17lte_defconfig
+#make Optikernel_a7y17lte_defconfig
 make -j
